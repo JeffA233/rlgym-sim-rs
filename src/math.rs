bdb8c392d423232816293a0a0eb12aca0841f205
JeffA233 <63136834+JeffA233@users.noreply.github.com>
2023-10-28 00:35:07 -0400

    remove extra math funcs, update cosine sim

--- src/math.rs
+++ src/math.rs
@@ -3,13 +3,13 @@
 
 // pub mod math{
 // use std::f32::consts::PI;
-use std::f32::consts::PI;
+// use std::f32::consts::PI;
 
 // use numpy::*;
-use ndarray::*;
+// use ndarray::*;
 use rand::{rngs::StdRng, *};
 
-use crate::gamestates::physics_object::{EulerAngle, Quaternion, RotationMatrix};
+use crate::gamestates::physics_object::RotationMatrix;
 
 // use crate::gamestates::physics_object::Quaternion;
 
@@ -40,35 +40,35 @@ pub fn vec_div_variable(a: &[f32], b: &f32) -> Vec<f32> {
 }
 
 /// multiply elementwise vec a * vec b
-pub fn element_mult_vec(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+pub fn element_mult_vec(a: &[f32], b: &[f32]) -> Vec<f32> {
     assert!(a.len() == b.len(), "length of a did not match length of b");
 
     std::iter::zip(a, b).map(|(x, y)| x * y).collect()
 }
 
 /// divide elementwise vec a / vec b
-pub fn element_div_vec(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+pub fn element_div_vec(a: &[f32], b: &[f32]) -> Vec<f32> {
     assert!(a.len() == b.len(), "length of a did not match length of b");
 
     std::iter::zip(a, b).map(|(x, y)| x / y).collect()
 }
 
 /// subtract elementwise vec b from vec a
-pub fn element_sub_vec(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+pub fn element_sub_vec(a: &[f32], b: &[f32]) -> Vec<f32> {
     assert!(a.len() == b.len(), "length of a did not match length of b");
 
     std::iter::zip(a, b).map(|(x, y)| x - y).collect()
 }
 
 /// add elementwise vec a + vec b
-pub fn element_add_vec(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+pub fn element_add_vec(a: &[f32], b: &[f32]) -> Vec<f32> {
     assert!(a.len() == b.len(), "length of a did not match length of b");
 
     std::iter::zip(a, b).map(|(x, y)| x + y).collect()
 }
 
 /// subtract elements of two vecs to get dist
-pub fn get_dist(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+pub fn get_dist(a: &[f32], b: &[f32]) -> Vec<f32> {
     element_sub_vec(a, b)
 }
 
@@ -106,7 +106,7 @@ pub fn norm_func(nums: &[f32]) -> f32 {
     nums.iter().map(|x| x.powi(2)).sum::<f32>().sqrt()
 }
 
-pub fn scalar_projection(vec: &Vec<f32>, dest_vec: &Vec<f32>) -> f32 {
+pub fn scalar_projection(vec: &[f32], dest_vec: &[f32]) -> f32 {
     let norm = norm_func(dest_vec);
     if norm == 0. {
         return 0.;
@@ -128,169 +128,152 @@ pub fn unitvec(vec: &[f32]) -> Vec<f32> {
 }
 
 pub fn cosine_similarity(a: Vec<f32>, b: Vec<f32>) -> f32 {
+    // FIXME: a full array of 0s may cause 0/0 which would be a NaN
     let a_norm = norm_func(&a);
     let b_norm = norm_func(&b);
 
-    // let mut a_vec: Vec<f64> = Vec::new();
-    // for i in a {
-    //     a_vec.push(i/a_norm);
-    // }
-
-    // let mut b_vec: Vec<f64> = Vec::new();
-    // for i in b {
-    //     b_vec.push(i/b_norm);
-    // }
     let a_vec = vec_div_variable(&a, &a_norm);
     let b_vec = vec_div_variable(&b, &b_norm);
 
-    let mut res: Vec<f32> = Vec::new();
-
-    // for (a, b) in a_vec.iter_mut().zip(b_vec.iter()) {
-    //     res.push(*a**b);
-    // }
-    for (a, b) in std::iter::zip(a_vec, b_vec) {
-        res.push(a * b);
-    }
-
-    return res.iter().sum();
+    std::iter::zip(a_vec, b_vec).map(|(a, b)| a * b).sum()
 }
 
-pub fn quat_to_euler(quat: &Vec<f32>) -> Vec<f32> {
-    assert!(quat.len() == 4, "quat is not the correct shape");
+// pub fn quat_to_euler(quat: &[f32]) -> Vec<f32> {
+//     assert!(quat.len() == 4, "quat is not the correct shape");
 
-    let w = quat[0];
-    let x = quat[1];
-    let y = quat[2];
-    let z = quat[3];
+//     let w = quat[0];
+//     let x = quat[1];
+//     let y = quat[2];
+//     let z = quat[3];
 
-    let sinr_cosp = 2. * (w * x + y * z);
-    let cosr_cosp = 1. - 2. * (x * x + y * y);
-    let sinp = 2. * (w * y - z * x);
-    let siny_cosp = 2. * (w * z + x * y);
-    let cosy_cosp = 1. - 2. * (y * y + z * z);
-    let roll = sinr_cosp.atan2(cosr_cosp);
+//     let sinr_cosp = 2. * (w * x + y * z);
+//     let cosr_cosp = 1. - 2. * (x * x + y * y);
+//     let sinp = 2. * (w * y - z * x);
+//     let siny_cosp = 2. * (w * z + x * y);
+//     let cosy_cosp = 1. - 2. * (y * y + z * z);
+//     let roll = sinr_cosp.atan2(cosr_cosp);
 
-    let yaw = siny_cosp.atan2(cosy_cosp);
+//     let yaw = siny_cosp.atan2(cosy_cosp);
 
-    let pitch = if sinp.abs() > 1. {
-        PI / 2.
-    } else {
-        sinp.asin()
-    };
+//     let pitch = if sinp.abs() > 1. {
+//         PI / 2.
+//     } else {
+//         sinp.asin()
+//     };
 
-    vec![-pitch, yaw, -roll]
-}
+//     vec![-pitch, yaw, -roll]
+// }
 
-/// quat Vec to rotation matrix Array2
-pub fn quat_to_rot_mtx(nums: &Vec<f32>) -> Array2<f32> {
-    let mut theta = Array2::<f32>::zeros((3, 3));
+// /// quat Vec to rotation matrix Array2
+// pub fn quat_to_rot_mtx(nums: &[f32]) -> Array2<f32> {
+//     let mut theta = Array2::<f32>::zeros((3, 3));
 
-    assert!(nums.len() == 4, "nums is not the correct shape");
+//     assert!(nums.len() == 4, "nums is not the correct shape");
 
-    let norm: f32 = nums
-        .clone()
-        .into_iter()
-        .map(|x: f32| x.powf(2.))
-        // .collect::<Vec<f64>>()
-        // .iter()
-        .sum();
+//     let norm: f32 = nums
+//         .clone()
+//         .into_iter()
+//         .map(|x: f32| x.powf(2.))
+//         // .collect::<Vec<f64>>()
+//         // .iter()
+//         .sum();
 
-    let w = -&nums[0];
-    let x = -&nums[1];
-    let y = -&nums[2];
-    let z = -&nums[3];
+//     let w = -&nums[0];
+//     let x = -&nums[1];
+//     let y = -&nums[2];
+//     let z = -&nums[3];
 
-    // let s: f64 = 1.0 / norm;
+//     // let s: f64 = 1.0 / norm;
 
-    if norm != 0. {
-        let s: f32 = 1.0 / norm;
+//     if norm != 0. {
+//         let s: f32 = 1.0 / norm;
 
-        theta[[0, 0]] = 1. - 2. * s * (y * y + z * z);
-        theta[[1, 0]] = 2. * s * (x * y + z * w);
-        theta[[2, 0]] = 2. * s * (x * z - y * w);
+//         theta[[0, 0]] = 1. - 2. * s * (y * y + z * z);
+//         theta[[1, 0]] = 2. * s * (x * y + z * w);
+//         theta[[2, 0]] = 2. * s * (x * z - y * w);
 
-        // left direction
-        theta[[0, 1]] = 2. * s * (x * y - z * w);
-        theta[[1, 1]] = 1. - 2. * s * (x * x + z * z);
-        theta[[2, 1]] = 2. * s * (y * z + x * w);
+//         // left direction
+//         theta[[0, 1]] = 2. * s * (x * y - z * w);
+//         theta[[1, 1]] = 1. - 2. * s * (x * x + z * z);
+//         theta[[2, 1]] = 2. * s * (y * z + x * w);
 
-        // up direction
-        theta[[0, 2]] = 2. * s * (x * z + y * w);
-        theta[[1, 2]] = 2. * s * (y * z - x * w);
-        theta[[2, 2]] = 1. - 2. * s * (x * x + y * y);
-    }
+//         // up direction
+//         theta[[0, 2]] = 2. * s * (x * z + y * w);
+//         theta[[1, 2]] = 2. * s * (y * z - x * w);
+//         theta[[2, 2]] = 1. - 2. * s * (x * x + y * y);
+//     }
 
-    theta
-}
+//     theta
+// }
 
-pub fn rotation_to_quaternion(m: RotationMatrix) -> Quaternion {
-    let trace = trace(&m);
-    let mut q = Quaternion::default();
+// pub fn rotation_to_quaternion(m: RotationMatrix) -> Quaternion {
+//     let trace = trace(&m);
+//     let mut q = Quaternion::default();
 
-    if trace > 0. {
-        let mut s = (trace + 1.).powf(0.5);
-        q.w = s * 0.5;
-        s = 0.5 / s;
-        q.x = (m.array[2][1] - m.array[1][2]) * s;
-        q.y = (m.array[0][2] - m.array[2][0]) * s;
-        q.z = (m.array[1][0] - m.array[0][1]) * s;
-    } else if m.array[0][0] >= m.array[1][1] && m.array[0][0] >= m.array[2][2] {
-        let s = (1. + m.array[0][0] - m.array[1][1] - m.array[2][2]).powf(0.5);
-        let inv_s = 0.5 / s;
-        q.x = 0.5 * s;
-        q.y = (m.array[1][0] + m.array[0][1]) * inv_s;
-        q.z = (m.array[2][0] + m.array[0][2]) * inv_s;
-        q.w = (m.array[2][1] - m.array[1][2]) * inv_s;
-    } else if m.array[1][1] > m.array[2][2] {
-        let s = (1. + m.array[1][1] - m.array[0][0] - m.array[2][2]).powf(0.5);
-        let inv_s = 0.5 / s;
-        q.x = (m.array[0][1] + m.array[1][0]) * inv_s;
-        q.y = 0.5 * s;
-        q.z = (m.array[1][2] + m.array[2][1]) * inv_s;
-        q.w = (m.array[0][2] - m.array[2][0]) * inv_s;
-    } else {
-        let s = (1. + m.array[2][2] - m.array[0][0] - m.array[1][1]).powf(0.5);
-        let inv_s = 0.5 / s;
-        q.x = (m.array[0][2] + m.array[2][0]) * inv_s;
-        q.y = (m.array[1][2] + m.array[2][1]) * inv_s;
-        q.z = 0.5 * s;
-        q.w = (m.array[1][0] - m.array[0][1]) * inv_s;
-    }
-    q.w = -q.w;
-    q.x = -q.x;
-    q.y = -q.y;
-    q.z = -q.z;
-    q
-}
+//     if trace > 0. {
+//         let mut s = (trace + 1.).powf(0.5);
+//         q.w = s * 0.5;
+//         s = 0.5 / s;
+//         q.x = (m.array[2][1] - m.array[1][2]) * s;
+//         q.y = (m.array[0][2] - m.array[2][0]) * s;
+//         q.z = (m.array[1][0] - m.array[0][1]) * s;
+//     } else if m.array[0][0] >= m.array[1][1] && m.array[0][0] >= m.array[2][2] {
+//         let s = (1. + m.array[0][0] - m.array[1][1] - m.array[2][2]).powf(0.5);
+//         let inv_s = 0.5 / s;
+//         q.x = 0.5 * s;
+//         q.y = (m.array[1][0] + m.array[0][1]) * inv_s;
+//         q.z = (m.array[2][0] + m.array[0][2]) * inv_s;
+//         q.w = (m.array[2][1] - m.array[1][2]) * inv_s;
+//     } else if m.array[1][1] > m.array[2][2] {
+//         let s = (1. + m.array[1][1] - m.array[0][0] - m.array[2][2]).powf(0.5);
+//         let inv_s = 0.5 / s;
+//         q.x = (m.array[0][1] + m.array[1][0]) * inv_s;
+//         q.y = 0.5 * s;
+//         q.z = (m.array[1][2] + m.array[2][1]) * inv_s;
+//         q.w = (m.array[0][2] - m.array[2][0]) * inv_s;
+//     } else {
+//         let s = (1. + m.array[2][2] - m.array[0][0] - m.array[1][1]).powf(0.5);
+//         let inv_s = 0.5 / s;
+//         q.x = (m.array[0][2] + m.array[2][0]) * inv_s;
+//         q.y = (m.array[1][2] + m.array[2][1]) * inv_s;
+//         q.z = 0.5 * s;
+//         q.w = (m.array[1][0] - m.array[0][1]) * inv_s;
+//     }
+//     q.w = -q.w;
+//     q.x = -q.x;
+//     q.y = -q.y;
+//     q.z = -q.z;
+//     q
+// }
 
-pub fn euler_to_rotation(pyr: EulerAngle) -> RotationMatrix {
-    let cp = pyr.pitch.cos();
-    let cy = pyr.yaw.cos();
-    let cr = pyr.roll.cos();
+// pub fn euler_to_rotation(pyr: EulerAngle) -> RotationMatrix {
+//     let cp = pyr.pitch.cos();
+//     let cy = pyr.yaw.cos();
+//     let cr = pyr.roll.cos();
 
-    let sp = pyr.pitch.sin();
-    let sy = pyr.yaw.sin();
-    let sr = pyr.roll.sin();
+//     let sp = pyr.pitch.sin();
+//     let sy = pyr.yaw.sin();
+//     let sr = pyr.roll.sin();
 
-    let mut theta = RotationMatrix::zeros();
+//     let mut theta = RotationMatrix::zeros();
 
-    // front
-    theta.array[0][0] = cp * cy;
-    theta.array[1][0] = cp * sy;
-    theta.array[2][0] = sp;
+//     // front
+//     theta.array[0][0] = cp * cy;
+//     theta.array[1][0] = cp * sy;
+//     theta.array[2][0] = sp;
 
-    // left
-    theta.array[0][1] = cy * sp * sr - cr * sy;
-    theta.array[1][1] = sy * sp * sr + cr * cy;
-    theta.array[2][1] = -cp * sr;
+//     // left
+//     theta.array[0][1] = cy * sp * sr - cr * sy;
+//     theta.array[1][1] = sy * sp * sr + cr * cy;
+//     theta.array[2][1] = -cp * sr;
 
-    // up
-    theta.array[0][2] = -cr * cy * sp - sr * sy;
-    theta.array[1][2] = -cr * sy * sp + sr * cy;
-    theta.array[2][2] = cp * cr;
+//     // up
+//     theta.array[0][2] = -cr * cy * sp - sr * sy;
+//     theta.array[1][2] = -cr * sy * sp + sr * cy;
+//     theta.array[2][2] = cp * cr;
 
-    theta
-}
+//     theta
+// }
 
 /// initializes a randomized Vec<f64> of length 3
 pub fn rand_uvec3() -> Vec<f32> {
