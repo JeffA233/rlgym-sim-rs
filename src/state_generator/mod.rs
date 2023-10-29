6fcdf6ccaf6aee94f643a19881ba91063983bff4
JeffA233 <63136834+JeffA233@users.noreply.github.com>
2023-10-29 01:38:12 -0400

    rename state_gen to combined_gen for clarity

--- src/state_generator/mod.rs
+++ src/state_generator/mod.rs
@@ -1,4 +1,3 @@
 pub mod combined_gen;
-pub mod state_gen;
 pub mod state_modifier;
 pub mod common_state_mods;
--- src/state_generator/state_modifier.rs
+++ src/state_generator/state_modifier.rs
@@ -1,5 +1,6 @@
 use crate::gamestates::game_state::GameState;
 
+/// default trait type for the modifiers that are used in things such as the CombinedStateGenerator
 pub trait StateModifier {
     fn modify_state(&mut self, state: &mut GameState);
 }
