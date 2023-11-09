046903a35ec50933569112f797011002cbbd23ef
JeffA233 <63136834+JeffA233@users.noreply.github.com>
2023-11-08 22:45:40 -0500

    add weighted setter

--- src/state_setters/mod.rs
+++ src/state_setters/mod.rs
@@ -2,3 +2,4 @@ pub mod default_state;
 pub mod random_state;
 pub mod state_setter;
 pub mod wrappers;
+pub mod weighted_state_setter;
---
+++ src/state_setters/weighted_state_setter.rs
@@ -0,0 +1,37 @@
+use rand::{distributions::{weighted::WeightedIndex, Distribution}, thread_rng, Rng, rngs::SmallRng, SeedableRng};
+
+use super::{state_setter::StateSetter, wrappers::state_wrapper::StateWrapper};
+
+/// weighted state setter that uses a rand distribution to poll for a choice
+pub struct WeightedSampleSetter {
+    state_setters: Vec<Box<dyn StateSetter + Send>>,
+    distribution: WeightedIndex<f64>,
+    rng: SmallRng,
+}
+
+impl WeightedSampleSetter {
+    pub fn new(state_setters: Vec<Box<dyn StateSetter + Send>>, weights: Vec<f64>, seed: Option<u64>) -> Self {
+        assert!(state_setters.len() == weights.len(), "WeightedSampleSetter requires the argument lengths match");
+        let distribution = WeightedIndex::new(&weights).unwrap();
+        let seed = match seed {
+            Some(seed) => seed,
+            None => thread_rng().gen_range(0..10000),
+        };
+        let rng = SmallRng::seed_from_u64(seed);
+        WeightedSampleSetter { state_setters, distribution, rng }
+    }
+}
+
+impl StateSetter for WeightedSampleSetter {
+    fn reset(&mut self, state_wrapper: &mut StateWrapper) {
+        let choice = self.distribution.sample(&mut self.rng);
+        self.state_setters[choice].reset(state_wrapper);
+    }
+
+    fn set_seed(&mut self, seed: u64) {
+        self.rng = SmallRng::seed_from_u64(seed);
+        for state_setter in &mut self.state_setters {
+            state_setter.set_seed(seed);
+        }
+    }
+}
